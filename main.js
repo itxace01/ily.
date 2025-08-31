f096b07955c60f2a446ffd0550880a1a58e4557f
itxace01 <shahkarkhan5811@gmail.com>
2025-09-01 02:59:51 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 /**
- * Ultimate Music Bot 
+ * ily 
  * Comprehensive Discord Bot
  * 
  * @fileoverview Core application
  * @version 1.0.0
- * @author GlaceYT
+ * @author itxace001
  */
 
 const DiscordClientFramework = require('discord.js').Client;
@@ -374,4 +374,5 @@ enterpriseApplicationManager.executeApplicationBootstrap();
 
 
 module.exports = enterpriseApplicationManager.clientRuntimeInstance;
+
 shiva.initialize(enterpriseApplicationManager.clientRuntimeInstance);
